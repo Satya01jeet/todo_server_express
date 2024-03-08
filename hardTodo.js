d8a31a403ca2249c3fd4fe9e6bebf6a599c8928e
Satya <sausatyajeet100@gmail.com>
2024-03-08 21:48:48 +0530

    init

--- hardTodo.js
+++ hardTodo.js
@@ -1,69 +1,72 @@
-const express = require('express')
-const fs = require('fs')
-const path = require('path')
-const cors = require('cors')
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get('/todos',(req,res) => {
-    try{
-        fs.readFile('todos.json','utf-8',(err,data) => {
-            if(err){
-                return res.status(500).send(`Error reading file`)
-            }
-            if(data == ""){
-                return res.json({});
-            }
-            res.json(JSON.parse(data))
-        })
-    }catch{
-        console.log(err)
-    }
-})
+app.get("/todos", (req, res) => {
+  try {
+    fs.readFile("todos.json", "utf-8", (err, data) => {
+      if (err) {
+        return res.status(500).send(`Error reading file`);
+      }
+      if (data == "") {
+        return res.json({});
+      }
+      res.json(JSON.parse(data));
+    });
+  } catch {
+    console.log(err);
+  }
+});
 
-app.post('/todos', (req, res) => {
-    const newTodo = {
-      id: Math.floor(Math.random() * 9000),
-      title: req.body.title,
-      description: req.body.description
-    };
+app.post("/todos", (req, res) => {
+  const newTodo = {
+    id: Math.floor(Math.random() * 9000),
+    title: req.body.title,
+    description: req.body.description,
+  };
+  try {
     fs.readFile("todos.json", "utf8", (err, data) => {
       if (err) throw err;
-      if(data == ""){
+      if (data == "") {
         var todos = [newTodo];
-      }
-      else{
-        var todos = JSON.parse(data)
-        todos.push(newTodo)
+      } else {
+        var todos = JSON.parse(data);
+        todos.push(newTodo);
       }
       fs.writeFile("todos.json", JSON.stringify(todos), (err) => {
         if (err) throw err;
         res.status(201).json(newTodo);
       });
     });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.delete("/todos/:id", (req, res) => {
+  fs.readFile("todos.json", "utf-8", (err, data) => {
+    if (err) throw err;
+    if (data == "") return res.send(`No todos to delete`);
+    var todos = JSON.parse(data);
+    const item = todos.find((todo) => todo.id == req.params.id);
+    if (item == null) return res.status(500).send(`invalid id`);
+    todos.splice(todos.indexOf(item), 1);
+    console.log(todos);
+    fs.writeFile("todos.json", JSON.stringify(todos), (err) => {
+      if (err) throw err;
+      res.status(201).send(`Deleted successfully`);
+    });
   });
+});
 
-app.delete('/todos/:id',(req,res) => {
-  fs.readFile('todos.json','utf-8',(err,data)=>{
-      if(err) throw err;
-      if(data == '') return res.send(`No todos to delete`)
-      var todos = JSON.parse(data)
-      const item = todos.find((todo) => todo.id == req.params.id)
-      if(item == null) return res.status(500).send(`invalid id`)
-      todos.splice(todos.indexOf(item),1);
-      console.log(todos)
-      fs.writeFile('todos.json', JSON.stringify(todos), (err) => {
-          if (err) throw err;
-          res.status(201).send(`Deleted successfully`)
-      })
-  })
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
-app.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname,'index.html'))
-})
-
-app.listen(3001)
+app.listen(3001);
--- index.js
+++ index.js
@@ -1,102 +1,98 @@
-const express = require('express')
-const app = express()
-const crypto = require('crypto')
-const { todo } = require('node:test')
+const express = require("express");
+const app = express();
+const crypto = require("crypto");
+const { todo } = require("node:test");
 
-app.use(express.json())
+app.use(express.json());
 
 var todoList = [
-    {
-        title: "todoServer",
-        completed: false,
-        description : "complete this todo list server",
-        id : "2222"
-    },
-    {
-        title: "walk5k",
-        completed: false,
-        description : "complete 5k steps in the evening",
-        id : "1111"
-    },
-]
-app.get('/todos', (req,res) => {
-    var todos = []
-    todoList.forEach((todo) => {
-        todos.push(todo.title)
-    })
-    res.status(200).json(todos)
-})
+  {
+    title: "todoServer",
+    completed: false,
+    description: "complete this todo list server",
+    id: "2222",
+  },
+  {
+    title: "walk5k",
+    completed: false,
+    description: "complete 5k steps in the evening",
+    id: "1111",
+  },
+];
+app.get("/todos", (req, res) => {
+  var todos = [];
+  todoList.forEach((todo) => {
+    todos.push(todo.title);
+  });
+  res.status(200).json(todos);
+});
 
-app.get('/todos/:id',((req,res) => {
-    try{
-        const item = todoList.find((item) => item.id === req.params.id)
-        if(item == null) res.status(404).send(`ID not found`)
-        res.status(200).json(item)
-    }
-    catch(err){
-        console.log(err)
-    }
-}))
+app.get("/todos/:id", (req, res) => {
+  try {
+    const item = todoList.find((item) => item.id === req.params.id);
+    if (item == null) res.status(404).send(`ID not found`);
+    res.status(200).json(item);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-var generatedIds = new Set()
+var generatedIds = new Set();
 
-function generateId(){
-    let id
-    do{
-        id = Math.floor(1000 + Math.random()*9000)
-    }while(generatedIds.has(id))
-    generatedIds.add(id)
-    return ''+id
+function generateId() {
+  let id;
+  do {
+    id = Math.floor(1000 + Math.random() * 9000);
+  } while (generatedIds.has(id));
+  generatedIds.add(id);
+  return "" + id;
 }
 
-app.post('/todos',(req,res) => {
-    try{
-        const item = req.body
-        const id = generateId();
-        const todoObj = {
-            "title": item.title,
-            "completed": item.completed,
-            "description": item.description,
-            "id": id
-        }
-        todoList.push(todoObj)
-        console.log(todoList)
-        res.status(201).json({"id":id})
-    }catch{
-        res.status(404).send()
-    }
-})
+app.post("/todos", (req, res) => {
+  try {
+    const item = req.body;
+    const id = generateId();
+    const todoObj = {
+      title: item.title,
+      completed: item.completed,
+      description: item.description,
+      id: id,
+    };
+    todoList.push(todoObj);
+    console.log(todoList);
+    res.status(201).json({ id: id });
+  } catch {
+    res.status(404).send();
+  }
+});
 
-app.put('/todos/:id',((req,res) => {
-    const updatedItem = req.body
-    try{
-        const item = todoList.find((item) => item.id === req.params.id)
-        if(item == null) res.status(404).send(`ID not found`)
-        else{
-            item.completed = updatedItem.completed
-            item.title = updatedItem.title
-            item.description = updatedItem.description
-            res.status(200).json(item)
-        }
+app.put("/todos/:id", (req, res) => {
+  const updatedItem = req.body;
+  try {
+    const item = todoList.find((item) => item.id === req.params.id);
+    if (item == null) res.status(404).send(`ID not found`);
+    else {
+      item.completed = updatedItem.completed;
+      item.title = updatedItem.title;
+      item.description = updatedItem.description;
+      res.status(200).json(item);
     }
-    catch(err){
-        console.log(err)
-    }
-}))
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-app.delete('/todos/:id',((req,res) => {
-    try{
-        const item = todoList.find((item) => item.id === req.params.id)
-        if(item == null) res.status(404).send(`ID not found`)
-        else{
-            todoList.splice(todoList.indexOf(item),1)
-            res.status(200).json(todoList)
-        }
+app.delete("/todos/:id", (req, res) => {
+  try {
+    const item = todoList.find((item) => item.id === req.params.id);
+    if (item == null) res.status(404).send(`ID not found`);
+    else {
+      todoList.splice(todoList.indexOf(item), 1);
+      res.status(200).json(todoList);
     }
-    catch(err){
-        console.log(err)
-    }
-}))
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-
-app.listen(3000, () => console.log(`Easy todo server running...`))
+app.listen(3000, () => console.log(`Easy todo server running...`));
